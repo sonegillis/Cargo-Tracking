3cd6450add172055276e65e820de2fd46e5e0467
sonegillis <sone_gillis@yahoo.com>
2020-12-26 10:21:09 +0100

    commit

--- cargotracking/settings.py
+++ cargotracking/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%5%4dm7k=a52e0&qhp$e1qh=lt4+rg#+8nw!tnfgo_(uefr9*e'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,10 +83,10 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cargo',
-        'USER': 'sonegillis',
-        'PASSWORD': 'mesogek1995',
-        'HOST': '127.0.0.1',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
     },
 }
 
